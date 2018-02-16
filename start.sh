4451a313837c5cd80bc5959318b04a7f381d13c4
Kevin Sillerud <kevin.sillerud@nav.no>
2018-02-16 13:22:43 +0100

    Add truststore to launch script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 eval "$(cat .env)"
 
-java -jar target/bankkontonummer-kanal-2.0-SNAPSHOT.jar
+java -Djavax.net.ssl.trustStore=$TRUSTSTORE_PATH -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASSWORD -jar target/bankkontonummer-kanal-2.0-SNAPSHOT.jar
