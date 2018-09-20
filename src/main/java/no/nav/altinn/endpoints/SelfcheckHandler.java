9374634c6bd3671e6316a9a2f6375bfe1e61f849
Joakim kartveit <joakim.kartveit@nav.no>
2018-09-20 08:49:45 +0200

    La til logging demo

--- src/main/java/no/nav/altinn/endpoints/SelfcheckHandler.java
+++ src/main/java/no/nav/altinn/endpoints/SelfcheckHandler.java
@@ -2,6 +2,8 @@ package no.nav.altinn.endpoints;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +12,14 @@ import java.io.PrintWriter;
 
 public class SelfcheckHandler extends AbstractHandler {
 
+    private final static Logger log = LoggerFactory.getLogger(SelfcheckHandler.class);
+
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
         if ("/is_alive".equals(target)) {
             handleIsAlive(response);
             baseRequest.setHandled(true);
+            log.info("Eg er i live, fra demo flokk");
         }
 
         if ("/is_ready".equals(target)) {
